68fbbfd934413e14f730ed182fd44c3a753a66e4
SuhutDev <Suhut.Wadiyo@outlook.com>
2022-11-30 15:00:07 +0700

    InterpretterPattern

---
+++ 16InterpreterPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using InterpreterPattern;
+
+IExpression isMale = InterpreterPatternDemo.GetMaleExpression();
+IExpression isMarriedWoman = InterpreterPatternDemo.GetMarriedWomanExpression();
+System.Console.WriteLine("John is male? " + isMale.Interpret("John"));
+System.Console.WriteLine("Julie is a married women? " + isMarriedWoman.Interpret("Married Julie"));
+
+
+public class InterpreterPatternDemo
+{
+    //Rule: Robert and John are male
+    public static IExpression GetMaleExpression()
+    {
+        IExpression robert = new TerminalExpression("Robert");
+        IExpression john = new TerminalExpression("John");
+        return new OrExpression(robert, john);
+    }
+
+    //Rule: Julie is a married women
+    public static IExpression GetMarriedWomanExpression()
+    {
+        IExpression julie = new TerminalExpression("Julie");
+        IExpression married = new TerminalExpression("Married");
+        return new AndExpression(julie, married);
+    }
+}
---
+++ 16InterpreterPattern/TerminalExpression.cs
@@ -0,0 +1,19 @@
+
+namespace InterpreterPattern;
+
+public class TerminalExpression : IExpression
+{
+    private string _data;
+    public TerminalExpression(string data)
+    {
+        _data = data;
+    }
+
+    public bool Interpret(string context)
+    {
+        if(context.Contains(_data)){
+         return true;
+      }
+      return false;
+    }
+}
